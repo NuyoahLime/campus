a0550e0b53b29ea1bc3c4e065f10fe445698e4f1
NuyoahLime <1014504110@qq.com>
2026-07-19 20:25:14 +0800

    feat(identity): wire secure password recovery

--- src/main/java/com/campusguinness/Application.java
+++ src/main/java/com/campusguinness/Application.java
@@ -2,12 +2,13 @@ package com.campusguinness;
 
 import com.campusguinness.infrastructure.bootstrap.BootstrapAdminProperties;
 import com.campusguinness.infrastructure.security.SecurityCorsProperties;
+import com.campusguinness.infrastructure.security.recovery.PasswordRecoveryProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({BootstrapAdminProperties.class, SecurityCorsProperties.class})
+@EnableConfigurationProperties({BootstrapAdminProperties.class, SecurityCorsProperties.class, PasswordRecoveryProperties.class})
 public class Application {
 
     public static void main(String[] args) {
